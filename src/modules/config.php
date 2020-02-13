90c8e6f193e152f34bdd605e3f2e27e619bf6325
Nik Coughlin <nrkn.com@gmail.com>
2020-02-14 12:34:46 +1300

    Pre module reorganisation

--- src/modules/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$WEB_ROOT = '/pvpoke/src/';
-$WEB_HOST = 'http://' . $_SERVER['HTTP_HOST'] . $WEB_ROOT;
-$UA = '';
-$GOOGLE_AD_CLIENT = '';
-?>
--- src/modules/leagueselect.php
+++
@@ -1,5 +0,0 @@
-<select class="league-select">
-	<option value="1500">Great League (CP 1500)</option>
-	<option value="2500">Ultra League (CP 2500)</option>
-	<option value="10000">Master League (Unlimited)</option>
-</select>
